c2dce88e00ffbc56b20a38e025cb7a5a58d948bd
e1turin <ivturnik2003@gmail.com>
2023-11-29 00:54:00 +0300

    capcha ddl

---
+++ coursework/sql/entities/capcha.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Capcha (
+    capcha_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    answer varchar(255),
+    picture_id integer NOT NULL,
+    CONSTRAINT "picture_id_fk" 
+    FOREIGN KEY (picture_id)
+        REFERENCES Pictures (picture_id)
+        ON DELETE CASCADE,
+);
