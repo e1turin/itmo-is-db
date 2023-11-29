993ca6b503d69592a8b0c46da5fd433d1ae0b808
Kimiega <dmbelitmo@mail.ru>
2023-11-29 04:32:47 +0300

    fix comment trigger

--- coursework/sql/triggers/creating_comment.sql
+++ coursework/sql/triggers/creating_comment.sql
@@ -1,20 +1,19 @@
 CREATE OR REPLACE FUNCTION create_reaction_set_for_comment()
-  RETURNS TRIGGER
-  LANGUAGE PLPGSQL
-  AS
+RETURNS TRIGGER
+AS
 $$
-DECLARE
-r_id INT;
-BEGIN
-    IF (NEW.reactions_id == NULL) THEN
-        r_id := INSERT INTO Reaction_sets() VALUES() RETURNING r_set_id;
-        UPDATE Comments
-            SET reactions_id = r_id
-            where comment_id = NEW.comment_id;
-    END IF;
-RETURN NEW;
-END;
-$$
+    DECLARE
+    r_id INT;
+    BEGIN
+        IF (NEW.reactions_id == NULL) THEN
+            r_id := INSERT INTO Reaction_sets() VALUES() RETURNING r_set_id;
+            UPDATE Comments
+                SET reactions_id = r_id
+                where comment_id = NEW.comment_id;
+        END IF;
+    RETURN NEW;
+    END;
+$$ LANGUAGE PLPGSQL;
 
 CREATE TRIGGER
     AFTER INSERT ON Comments
