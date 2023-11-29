6ad2749030672e2e284a4ddc860889c237cbb345
Kimiega <dmbelitmo@mail.ru>
2023-11-29 14:03:44 +0300

    update select queries

--- coursework/sql/queries/add_comment.sql
+++ coursework/sql/queries/add_comment.sql
@@ -37,3 +37,9 @@ VALUES ('Privet, Misha!', 7);
 insert into "Comments" (content, user_id)
 VALUES ('Privet, SuperHot!', 8);
 
+insert into "Comments" (content, user_id)
+VALUES ('Privet, SuperHot!', 8);
+
+INSERT INTO "Comments" (title,content,user_id)
+VALUES ('THE TOP-1 COMMENT', 'There is nothing', 6);
+
