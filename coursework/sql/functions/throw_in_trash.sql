bca5c513b8d8238db26df937addcf85863f34dd5
Kimiega <dmbelitmo@mail.ru>
2023-11-29 04:15:57 +0300

    throw in trash function

--- coursework/sql/functions/throw_in_trash.sql
+++ coursework/sql/functions/throw_in_trash.sql
@@ -1,1 +1,5 @@
-/* TODO */
+CREATE OR REPLACE PROCEDURE throw_in_trash(comment_id integer, reason text)
+LANGUAGE SQL
+AS $$
+    INSERT INTO Trash(comment_id, reason) VALUES(comment_id, reason);
+$$;
