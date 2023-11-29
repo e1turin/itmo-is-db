37c5673c9e4a0f43a31fae5093593335f34305a9
Kimiega <dmbelitmo@mail.ru>
2023-11-29 04:45:38 +0300

    fix naming tables in functions

--- coursework/sql/functions/throw_in_trash.sql
+++ coursework/sql/functions/throw_in_trash.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE PROCEDURE throw_in_trash(comment_id integer, reason text)
 LANGUAGE SQL
 AS $$
-    INSERT INTO Trash(comment_id, reason) VALUES(comment_id, reason);
+    INSERT INTO "Trash"(comment_id, reason) VALUES(comment_id, reason);
 $$;
