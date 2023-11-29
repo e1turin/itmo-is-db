4cbfbc4da16760b302b7b1e8c606020b4b9253f8
Kimiega <dmbelitmo@mail.ru>
2023-11-29 05:41:40 +0300

    trigger comment last fix

--- coursework/sql/triggers/creating_comment.sql
+++ coursework/sql/triggers/creating_comment.sql
@@ -3,16 +3,22 @@ RETURNS TRIGGER
 AS
 $$
     DECLARE
-    r_id INT;
+    r_id integer := 0;
     BEGIN
-        IF (NEW.reactions_id == NULL) THEN
-            r_id := INSERT INTO "Reaction_sets" VALUES() RETURNING r_set_id;
-            UPDATE "Comments"
+        IF (NEW.reactions_id IS NULL) THEN
+            with tempa as (
+                INSERT INTO "Reaction_sets"
+                    DEFAULT VALUES
+                    RETURNING r_set_id as id
+            )
+            SELECT SUM(tempa.id) INTO r_id
+            FROM tempa;
+           UPDATE "Comments"
                 SET reactions_id = r_id
                 where comment_id = NEW.comment_id;
         END IF;
     RETURN NEW;
-    END;
+    END
 $$ LANGUAGE PLPGSQL;
 
 CREATE TRIGGER after_inserting_comment
