8ec8d1af3e2849aa291455a0556b3871b310d21e
Kimiega <dmbelitmo@mail.ru>
2023-11-29 05:45:39 +0300

    add query to insert comment without rs

--- coursework/sql/queries/add_comment.sql
+++ coursework/sql/queries/add_comment.sql
@@ -16,3 +16,17 @@ with rs as (
 insert into "Comments" (content, user_id, reactions_id)
 	select 'Some interesting content', u.id, rs.id
 	from rs, u;
+
+with rl as (
+    select role_id as id
+    from "Roles"
+    where name = 'loh'
+),  u as (
+insert into "Users" (username, isu_id, role_id, password)
+select 'kek', 335046, rl.id, 'kek'
+from rl
+         returning user_id as id
+)
+insert into "Comments" (content, user_id)
+select 'Some interesting content', u.id
+from u;
