8f38ea0e4b17cf700a802a56c323561616ce8608
e1turin <ivturnik2003@gmail.com>
2023-12-06 00:26:36 +0300

    fix user role default

--- coursework/sql/entities/users.sql
+++ coursework/sql/entities/users.sql
@@ -2,6 +2,7 @@ CREATE TABLE "Users" (
     user_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     username varchar(255) NOT NULL UNIQUE,
     isu_id integer NOT NULL UNIQUE,
-    role_id integer NOT NULL, -- Default 0
+    role_id integer DEFAULT NULL,
     password varchar(255) NOT NULL
 );
+
