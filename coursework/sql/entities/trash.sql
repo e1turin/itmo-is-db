bf36f29cd30e0166de44258c8aeae00c478c1b3b
e1turin <ivturnik2003@gmail.com>
2023-12-06 02:38:49 +0300

    add recycle date on trash

--- coursework/sql/entities/trash.sql
+++ coursework/sql/entities/trash.sql
@@ -1,5 +1,6 @@
 CREATE TABLE "Trash" (
     trash_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     comment_id integer NOT NULL,
-    reason text
+    reason text,
+    recycle_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
