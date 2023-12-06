fed58996e3a5379c324feb855d7b648a2e526035
e1turin <ivturnik2003@gmail.com>
2023-12-06 04:33:57 +0300

    add appending reactions procedure

---
+++ coursework/sql/functions/append_reaction.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE PROCEDURE append_reaction_to_comment(reaction_text varchar, c_id Integer)
+LANGUAGE PLPGSQL
+AS $$
+    DECLARE
+        r_id int;
+        r_cnt int;
+    BEGIN
+        IF length(reaction_text) < 7 THEN
+            SELECT reactions_id 
+            INTO r_id
+            FROM "Comments" 
+            WHERE "Comments".comment_id = append_reaction_to_comment.c_id;
+
+			SELECT reactions -> reaction_text
+			into r_cnt
+            FROM "Reaction_sets" 
+            WHERE r_set_id = r_id;
+
+			if r_cnt is null then
+                r_cnt := 0;
+			end if;
+
+            UPDATE "Reaction_sets"
+            SET reactions = jsonb_set(reactions, array[reaction_text], to_jsonb(r_cnt + 1), TRUE)
+            WHERE r_set_id = r_id;
+
+        END IF;
+    end;
+$$;
+
+/* EXAMPLE
+  call append_reaction_to_comment('tl;dr',  15) 
+*/
